b0ed2226bea876c5909452f3dbce655ac9038d18
handhmw <handrihmw@gmail.com>
2020-03-07 13:09:54 +0700

    update icon

--- public/js/app.js
+++ public/js/app.js
@@ -59344,8 +59344,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/niagaspa/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/niagaspa/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/spa-nghstr/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/spa-nghstr/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
